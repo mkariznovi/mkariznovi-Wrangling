345fda2553652168508326351908e039f9213648
mkariznovi <mkariznovy2000@gmail.com>
2020-05-18 17:50:49 -0600

    Final part of program

--- First_code.R
+++ First_code.R
@@ -108,8 +108,98 @@ library(rvest)
 # tab_3 <- full_join(tab_1,tab_2,by="Team")
 library(rvest)
 library(tidyverse)
-url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
-h <- read_html(url)
-tab <-html_nodes(h,"table")
-T <- html_table(tab[[5]],fill=TRUE)
-head(T)
+# url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+# h <- read_html(url)
+# tab <-html_nodes(h,"table")
+# T <- html_table(tab[[5]],fill=TRUE)
+# head(T)
+# s <- '5\'10"'
+# cat(s)
+# # murders_raw was defined in the web scraping section
+# 
+# # detect whether there are commas
+# commas <- function(x) any(str_detect(x, ","))
+# murders_raw %>% summarize_all(funs(commas))
+# 
+# # replace commas with the empty string and convert to numeric
+# test_1 <- str_replace_all(murders_raw$population, ",", "")
+# test_1 <- as.numeric(test_1)
+# 
+# # parse_number also removes commas and converts to numeric
+# test_2 <- parse_number(murders_raw$population)
+# identical(test_1, test_2)
+# 
+# murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
+# murders_new %>% head
+# cat(" LeBron James is 6’8\" ")
+# ulr <-"https://www.worldometers.info/coronavirus/"
+# covid <- html_nodes(read_html(ulr),"table")
+# Covid_table <- html_table(covid[[1]])
+# covid_my_table <- Covid_table%>% mutate_at(3:14,parse_number)
+# data("reported_heights")
+# class(reported_heights)
+# x <- as.numeric(reported_heights$height)
+# head(x)
+# sum(is.na(x))
+# reported_heights %>% mutate(new_height = as.numeric(height)) %>%
+#   filter(is.na(new_height)) %>% 
+#   head(n=10)
+# pattern <- "^\\d$"
+# yes <- c("1", "5", "9")
+# no <- c("12", "123", " 1", "a4", "b")
+# str_view(c(yes, no), pattern)
+s <- c("70","5 ft","4'11","",".","Six feet")
+# pattern <- "\\d|ft"
+# str_view_all(s,pattern)
+# animals <- c("cat", "puppy", "Moose", "MONKEY")
+# pattern <- "[a-z]"
+# str_detect(animals, pattern)
+# animals <- c("cat", "puppy", "Moose", "MONKEY")
+# pattern <- "^[A-Z]"
+# str_detect(animals, pattern)
+# animals <- c("cat", "puppy", "Moose", "MONKEY")
+# pattern <- "[a-z]{4,5}"
+# str_detect(animals, pattern)
+# animals <- c("moose", "monkey", "meerkat", "mountain lion")
+# pattern <- “mo+”
+# # str_detect(animals, pattern)
+# problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+# pattern_with_groups <- "^([4-7])[,\\.\\s](\\d*)$"
+# str_replace(problems, pattern_with_groups, "\\1'\\2")
+# s <- c("5'10", "6'1\"", "5'8inches", "5'7.5")
+# tab <- data.frame(x = s)
+# extract(data = tab, col = x, into = c(“feet”, “inches”, “decimal”), 
+# #         regex = "(\\d)'(\\d{1,2})(\\.)?"
+# library(rvest)
+# library(tidyverse)
+# library(stringr)
+# url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+# tab <- read_html(url) %>% html_nodes("table")
+# polls <- tab[[5]] %>% html_table
+# names(polls) <- c("dates", "remain", "leave", "undecided", "lead", "samplesize", "pollster", "poll_type", "notes")
+# polls <- polls[str_detect(polls$remain, "%"), -9]
+# nrow(polls)
+# as.numeric(str_replace(polls$remain, "%", ""))/100
+# str_replace(polls$undecided,"N/A","0")
+# library(dslabs)
+# library(lubridate)
+# options(digits = 3) 
+# data(brexit_polls)
+# !is.na(brexit_polls$startdate)
+# x <- round_date(brexit_polls$enddate,unit = "week")
+# sum(month(brexit_polls$startdate) == "4")
+# sum(x=="2016-06-12")
+# week_day <- weekdays(brexit_polls$enddate)
+# data("movielens")
+# dates <- as_datetime(movielens$timestamp)
+# reviews_by_year <- table(year(dates))    # count reviews by year
+# names(which.max(reviews_by_year))    # name of year with most reviews
+install.packages("tidytext","gutenbergr")
+library(tidyverse)
+library(gutenbergr)
+library(tidytext)
+options(digits = 3)
+data("gutenberg_metadata")
+gutenberg_metadata %>%
+  filter(str_detect(title, "Pride and Prejudice"))  %>%
+  summarise(n = n_distinct(gutenberg_id)) %>%gutenberg_works()
